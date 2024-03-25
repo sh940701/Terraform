056c5683d4bfdbe27b1711745de1d11745b222bf
sh940701 <visioner2168@gmail.com>
2024-03-25 22:09:22 +0900

    learn-terraform-locals

---
+++ Configuration_Language/learn-terraform-locals/terraform.tf
@@ -0,0 +1,21 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  cloud {
+    organization = "Commelier"
+    workspaces {
+      name = "learn-terraform-locals"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
+    }
+  }
+
+  required_version = ">= 1.1"
+}
+
