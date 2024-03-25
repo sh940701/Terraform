cf1ad37ecbebd8adc6bb39f4da105bb81c38da46
sh940701 <visioner2168@gmail.com>
2024-03-25 22:09:19 +0900

    learn-terraform-functions

---
+++ Configuration_Language/learn-terraform-functions/terraform.tf
@@ -0,0 +1,21 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  cloud {
+    organization = "Commelier"
+    workspaces {
+      name = "learn-terraform-functions"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
---
+++ Configuration_Language/learn-terraform-functions/variables.tf
@@ -0,0 +1,39 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default     = "10.1.0.0/16"
+}
+
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default     = "10.1.0.0/24"
+}
+
+variable "environment_tag" {
+  description = "Environment tag"
+  default     = "Learn"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy your instance"
+  default     = "ap-northeast-2"
+}
+
+variable "user_name" {
+  description = "The user creating this infrastructure"
+  default     = "terraform"
+}
+
+variable "user_department" {
+  description = "The organization the user belongs to: dev, prod, qa"
+  default     = "learn"
+}
+
+variable "aws_amis" {
+  type    = map(string)
+  default = {
+    "ap-northeast-2" = "ami-09a7535106fbd42d5"
+  }
+}
