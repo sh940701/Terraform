5004f59cdbb833b9b15d03c80055233b329bbf28
sh940701 <visioner2168@gmail.com>
2024-03-25 22:10:40 +0900

    learn-terraform-sensitive-variables

---
+++ Configuration_Language/learn-terraform-sensitive-variables/terraform.tf
@@ -0,0 +1,19 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  cloud {
+    organization = "Commelier"
+    workspaces {
+      name = "learn-terraform-sensitive-variables"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
+    }
+  }
+  required_version = ">= 1.2"
+}
